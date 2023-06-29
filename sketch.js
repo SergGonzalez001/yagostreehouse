677895f9f4932dff52d01cdb192d5442e7401f84
SergGonzalez001 <138155461+SergGonzalez001@users.noreply.github.com>
2023-06-29 14:26:56 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,120 @@
+//The setup function only happens once
+var sandy =5
+function setup() {
+	createCanvas(500, 500); //create a 500px X 500px canvas
+}
+
+//The draw function happens over and over again
+function draw() {
+  background(248, 7, 248); //an RGB color for the canvas' background
+  //circle
+  stroke(255,255,127) // an RGB color for the circle's border
+  fill(255,255,127,150); // an RGB color for the inside of the circle (the last number refers to transparency (min. 0, max. 255))
+  triangle (30, sandy, 75, 58, 20, 86, 75); // center of canvas, 20px dia
+
+ stroke(255,255,127) // an RGB color for the circle's border
+  fill(255,255,127,150); // an RGB color for the inside of the circle (the last number refers to transparency (min. 0, max. 255))
+  triangle (50, sandy, 75, 78, 20, 96, 75); // center of canvas, 20px dia
+
+ stroke(255,255,127) // an RGB color for the circle's border
+  fill(255,255,127,150); // an RGB color for the inside of the circle (the last number refers to transparency (min. 0, max. 255))
+  triangle (50, sandy, 95, 78, 40, 96, 95); // center of canvas, 20px dia
+
+  stroke(255,255,127) // an RGB color for the circle's border
+  fill(255,255,127,150); // an RGB color for the inside of the circle (the last number refers to transparency (min. 0, max. 255))
+  triangle (100, sandy, 75, 108, 20, 166, 75); // center of canvas, 20px dia
+
+ stroke(255,255,127) // an RGB color for the circle's border
+  fill(255,255,127,150); // an RGB color for the inside of the circle (the last number refers to transparency (min. 0, max. 255))
+  triangle (120, sandy, 75, 128, 20, 356, 75); // center of canvas, 20px dia
+
+ stroke(255,255,127) // an RGB color for the circle's border
+  fill(255,255,127,150); // an RGB color for the inside of the circle (the last number refers to transparency (min. 0, max. 255))
+  triangle (50, sandy, 95, 78, 40, 96, 95); // center of canvas, 20px dia
+
+  stroke(255,255,127) // an RGB color for the circle's border
+  fill(255,255,127,150); // an RGB color for the inside of the circle (the last number refers to transparency (min. 0, max. 255))
+  triangle (30, sandy, 375, 458, 20, 586, 75); // center of canvas, 20px dia
+
+ stroke(255,255,127) // an RGB color for the circle's border
+  fill(255,255,127,150); // an RGB color for the inside of the circle (the last number refers to transparency (min. 0, max. 255))
+  triangle (50, sandy, 275, 78, 220, 96, 275); // center of canvas, 20px dia
+
+ stroke(255,255,127) // an RGB color for the circle's border
+  fill(255,255,127,150); // an RGB color for the inside of the circle (the last number refers to transparency (min. 0, max. 255))
+  triangle (50, sandy, 195, 78, 140, 96, 195); // center of canvas, 20px dia
+
+  stroke(255,255,127) // an RGB color for the circle's border
+  fill(255,255,127,150); // an RGB color for the inside of the circle (the last number refers to transparency (min. 0, max. 255))
+  triangle (200, sandy, 175, 208, 120, 266, 175); // center of canvas, 20px dia
+
+ stroke(255,255,127) // an RGB color for the circle's border
+  fill(255,255,127,150); // an RGB color for the inside of the circle (the last number refers to transparency (min. 0, max. 255))
+  triangle (120, sandy, 1605, 128, 1700, 356, 1775); // center of canvas, 20px dia
+
+ stroke(255,255,127) // an RGB color for the circle's border
+  fill(255,255,127,150); // an RGB color for the inside of the circle (the last number refers to transparency (min. 0, max. 255))
+  triangle (1050, sandy, 305, 1078, 400, 1096, 505); // center of canvas, 20px dia
+
+
+  stroke(255,255,127) // an RGB color for the circle's border
+  fill(255,255,127,150); // an RGB color for the inside of the circle (the last number refers to transparency (min. 0, max. 255))
+  triangle (1130, sandy, 75, 1158, 20, 1186, 75); // center of canvas, 20px dia
+
+ stroke(255,255,127) // an RGB color for the circle's border
+  fill(255,255,127,150); // an RGB color for the inside of the circle (the last number refers to transparency (min. 0, max. 255))
+  triangle (50, sandy, 1175, 78, 1120, 96, 1175); // center of canvas, 20px dia
+
+ stroke(255,255,127) // an RGB color for the circle's border
+  fill(255,255,127,150); // an RGB color for the inside of the circle (the last number refers to transparency (min. 0, max. 255))
+  triangle (1250, sandy, 95, 1278, 40, 1296, 95); // center of canvas, 20px dia
+
+  stroke(255,255,127) // an RGB color for the circle's border
+  fill(255,255,127,150); // an RGB color for the inside of the circle (the last number refers to transparency (min. 0, max. 255))
+  triangle (100, sandy, 1275, 108, 1220, 166, 1275); // center of canvas, 20px dia
+
+ stroke(255,255,127) // an RGB color for the circle's border
+  fill(255,255,127,150); // an RGB color for the inside of the circle (the last number refers to transparency (min. 0, max. 255))
+  triangle (1320, sandy, 75, 1328, 20, 1356, 75); // center of canvas, 20px dia
+
+ stroke(255,255,127) // an RGB color for the circle's border
+  fill(255,255,127,150); // an RGB color for the inside of the circle (the last number refers to transparency (min. 0, max. 255))
+  triangle (50, sandy, 1395, 78, 1340, 96, 1395); // center of canvas, 20px dia
+
+  stroke(255,255,127) // an RGB color for the circle's border
+  fill(255,255,127,150); // an RGB color for the inside of the circle (the last number refers to transparency (min. 0, max. 255))
+  triangle (1430, sandy, 375, 1458, 20, 1586, 75); // center of canvas, 20px dia
+
+ stroke(255,255,127) // an RGB color for the circle's border
+  fill(255,255,127,150); // an RGB color for the inside of the circle (the last number refers to transparency (min. 0, max. 255))
+  triangle (50, sandy, 2275, 78, 2220, 96, 1275); // center of canvas, 20px dia
+
+ stroke(255,255,127) // an RGB color for the circle's border
+  fill(255,255,127,150); // an RGB color for the inside of the circle (the last number refers to transparency (min. 0, max. 255))
+  triangle (2250, sandy, 195, 2278, 140, 2296, 195); // center of canvas, 20px dia
+
+  stroke(255,255,127) // an RGB color for the circle's border
+  fill(255,255,127,150); // an RGB color for the inside of the circle (the last number refers to transparency (min. 0, max. 255))
+  triangle (200, sandy, 1475, 208, 1420, 266, 1475); // center of canvas, 20px dia
+
+ stroke(255,255,127) // an RGB color for the circle's border
+  fill(255,255,127,150); // an RGB color for the inside of the circle (the last number refers to transparency (min. 0, max. 255))
+  triangle (1520, sandy, 1605, 1528, 1700, 2356, 1775); // center of canvas, 20px dia
+
+ stroke(255,255,127) // an RGB color for the circle's border
+  fill(255,255,127,150); // an RGB color for the inside of the circle (the last number refers to transparency (min. 0, max. 255))
+  triangle (1050, sandy, 2305, 1078, 2400, 1096, 2505); // center of canvas, 20px dia
+
+
+
+
+sandy=sandy+1
+}
+
+function mousePressed(){
+if (sandy>=250){
+  sandy=5;
+}else{
+  sandy=sandy+10;
+}
+}
